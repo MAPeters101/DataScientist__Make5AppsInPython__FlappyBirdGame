5ad7615be69b8f59603e8eb3c83d6c0f6a6e825f
Mark Peters <mapeters101@earthlink.net>
2025-02-10 16:43:01 -0500

    Complete work on lesson073 for FlappyBird.  main_gameplay().

--- flappy.py
+++ flappy.py
@@ -60,7 +60,25 @@ def main_gameplay():
         {'x':screen_width+200+(screen_width/2),'y':n_pip2[1]['y']},
     ]
 
+    pip_Vx = -4
+    p_vx = -9
+    p_mvx = 10
+    p_mvy = -8
+    p_accuracy = 1
 
+    p_flap_accuracy = -8
+    p_flap = False
+
+    while True:
+        for event in pygame.event.get():
+            if event.type==QUIT or (event.type==KEYDOWN and event.key==K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+            if event.type==KEYDOWN and (event.key==K_SPACE or event.key==K_UP):
+                if p_y > 0:
+                    p_vx = p_flap_accuracy
+                    p_flap = True
+                    game_audio_sound['wing'].play()
 
 
 
